aa1b99f73dd53d93bc47d56c0f0c25954fe9e090
Osmond1689 <osmond1689@gmail.com>
2020-06-27 19:47:11 +0800

    单线程判断流情况

--- client/video_monitor.py
+++ client/video_monitor.py
@@ -1,14 +1,18 @@
 import urllib.request,json,os
-def Video_monitor(url):
-    try:
-        req=urllib.request.urlopen(url)
-    #捕捉异常
-    except TypeError as e:
-        return 0,e
-    except urllib.error.HTTPError:
-        return 1,404
-    else:
-        return 1,req.getcode()
+def Huoqu_status(ztxx):
+    ztadds=ztxx[1]
+    ztstatus=[]
+    for url in ztadds:
+        try:
+            req=urllib.request.urlopen(url)
+        #捕捉异常
+        except TypeError as e:
+            return e
+        except urllib.error.HTTPError:
+            ztstatus.append(404)
+        else:
+            ztstatus.append(req.getcode())
+    return ztstatus
 def Readjson(path):
     #读取json视频列表为对象
     try:
@@ -16,17 +20,26 @@ def Readjson(path):
             data = json.load(f)
     except FileNotFoundError as e:
         return e,None
+    except json.decoder.JSONDecodeError as e:
+        return e,None
     #拿到第一次嵌套的list
     else:
-    #遍历拿到第二层嵌套的dict返回
-        return None,data.get("list")
-def main():
-    print(1)
-    path="video_list.json"   
+    #遍历拿到第二层嵌套的dict返回 
+        list=data.get("list")
+        ztname=[]
+        ztadds=[]
+        for dict in list:
+            for x in dict:
+                ztname.append(x)
+                ztadds.append(dict.get(x))
+                ztxx=[ztname,ztadds]
+        return None,ztxx
+if __name__ == "__main__":
+    path="video_list.json"
     req=Readjson(path)
-    # list=[1,]
-    # if req[0] == None:
-    #     list=req[1]
-    # else:
-    #     print (req[0])
-    print(req)
+    if req[0] == None:
+        ztstatus=Huoqu_status(req[1])
+    else:
+        print (req[0])
+    for i in range(len(ztstatus)):
+        print(req[1][0][i],req[1][1][i],ztstatus[i])
